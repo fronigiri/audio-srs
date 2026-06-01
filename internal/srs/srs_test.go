c60e3b3b2d1991157a721b7f36cd0425a58b1c62
fronigiri <addihor97@gmail.com>
2026-05-31 22:50:19 -0400

    added more potential test cases

--- internal/srs/srs_test.go
+++ internal/srs/srs_test.go
@@ -16,10 +16,20 @@ func testScheduler(t *testing.T) {
 		},
 		// forgotten card
 		{
-			card:   Card{Repetition: 5, Interval: 1, EF: 2.5},
+			card:   Card{Repetition: 5, Interval: 4, EF: 2.5},
 			rating: 2,
 		},
 		// ease factor below 1.3
+		{
+			card:   Card{Repetition: 5, Interval: 1, EF: 0.9},
+			rating: 3,
+		},
+		//first successful review should be 1 day
+		{
+			card:   Card{Repetition: 5, Interval: 1, EF: 0.9},
+			rating: 3,
+		},
+		//second successful review should be 6 days
 		{
 			card:   Card{Repetition: 5, Interval: 1, EF: 0.9},
 			rating: 3,
