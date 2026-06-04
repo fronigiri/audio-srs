5007264611bbaa447eb9d3ed87d01fe9b433d64d
fronigiri <addihor97@gmail.com>
2026-06-03 22:45:34 -0400

    more test stuff

--- internal/srs/srs_test.go
+++ internal/srs/srs_test.go
@@ -8,33 +8,47 @@ func testScheduler(t *testing.T) {
 	cases := []struct {
 		card        Card
 		rating      int
-		expectedRep int
+		expectedInterval int
 		expectedEF  int
 	}{
 		// normal test
 		{
-			card:   Card{Repetition: 0, Interval: 1, EF: 2.5},
+			card:   Card{Repetition: 5, Interval: 4, EF: 2.5},
 			rating: 4,
+			expectedInterval: 1,
+			expectedEF: 
 		},
 		// forgotten card
 		{
-			card:   Card{Repetition: 5, Interval: 4, EF: 2.5},
+			card:   Card{Repetition: 1, Interval: 6, EF: 2.5},
 			rating: 2,
+			expectedInterval: 1,
+			expectedEF:
 		},
 		// ease factor below 1.3
 		{
 			card:   Card{Repetition: 5, Interval: 1, EF: 0.9},
 			rating: 3,
+			
 		},
 		//first successful review should be 1 day
 		{
-			card:   Card{Repetition: 5, Interval: 1, EF: 0.9},
+			card:   Card{Repetition: 0, Interval: 1, EF: 0.9},
 			rating: 3,
+			expectedInterval: 1,
+			expectedEF:
 		},
 		//second successful review should be 6 days
 		{
-			card:   Card{Repetition: 5, Interval: 1, EF: 0.9},
+			card:   Card{Repetition: 1, Interval: 1, EF: 0.9},
 			rating: 3,
+			expectedInterval: 6,
+			expectedEF:
 		},
+	}
+	for _, c := range cases{
+		review(c)
+
+
 	}
 }
