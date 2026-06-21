babbc62ca20d66072bf907a0adceb6ccd85a08d5
fronigiri <addihor97@gmail.com>
2026-06-20 22:59:50 -0400

    added create deck logic

--- internal/database/decks.go
+++ internal/database/decks.go
@@ -2,10 +2,23 @@ package database
 
 import (
 	"time"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
 type Deck struct {
 	ID        int
 	Name      string
 	CreatedAt time.Time
+}
+
+func (db *DB) CreateDeck(d Deck) error {
+	_, err := db.conn.Exec(`
+	INSERT INTO decks (id, name, created_at) VALUES (?,?,?)
+	`,
+		d.ID,
+		d.Name,
+		d.CreatedAt.Format(time.RFC3339),
+	)
+	return err
 }
