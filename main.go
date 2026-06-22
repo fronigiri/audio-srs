f91f6164f394adba6d2f27ce863921a71c9b5a25
fronigiri <addihor97@gmail.com>
2026-06-21 23:47:00 -0400

    change to main.go

--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/fronigiri/audio-srs/internal/database"
 )
 
 func main() {
+	fmt.Print("welcome")
 	db, err := database.StartDB()
 	if err != nil {
 		log.Fatal(err)
